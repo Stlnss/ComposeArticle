0c31afad5ed192f2cdf65e4567fffd5a39e58f5f
stlnss <stlnesslv@gmail.com>
2023-04-15 14:54:02 +1000

    Create ArticleCard UI

--- app/src/main/java/com/still/composearticle/MainActivity.kt
+++ app/src/main/java/com/still/composearticle/MainActivity.kt
@@ -3,13 +3,23 @@ package com.still.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.still.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticleCardApp()
                 }
             }
         }
@@ -30,14 +40,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeArticleCardApp(modifier: Modifier = Modifier) {
+    ArticleCard(
+        modifier = modifier,
+        headerImage = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(id = R.string.article_title),
+        overView = stringResource(id = R.string.article_overview),
+        body = stringResource(id = R.string.article_body)
+    )
+}
+
+@Composable
+fun ArticleCard(
+    modifier: Modifier,
+    headerImage: Painter,
+    title: String,
+    overView: String,
+    body: String
+) {
+    Column() {
+        Image(
+            painter = headerImage,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = overView,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ComposeArticleCardApp()
     }
 }
